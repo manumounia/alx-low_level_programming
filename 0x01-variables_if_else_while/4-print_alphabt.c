e481da7bcb1d1e19d5ac69fa21f5ffe2f989aa45
manumounia <doudou.mounia100@gmail.com>
2023-06-16 11:06:03 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,23 +2,21 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * except q and e
- * followed by a new line
- * Return: Always 0 (Success)
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
-	char alph;
-
+	char alph = 'a';
 
 	while (alph <= 'z')
 {
 		if (alph != 'e' && alph != 'q')
-		{
+{
 		putchar(alph);
-		}
+}
 		alph++;
 }
 	putchar('\n');
-	return  (0);
+	return (0);
 }
