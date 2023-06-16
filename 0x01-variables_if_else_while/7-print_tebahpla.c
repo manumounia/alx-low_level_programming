fa160042491f2bd1849b7c70e6c8792d2ff74224
manumounia <doudou.mounia100@gmail.com>
2023-06-16 13:57:55 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph <= 'a'; alph--)
+{
+	putchar(alph);
+}
+	putchar('\n');
+	return  (0);
+}
