3e98b3ef7b08211fe475de8fd04428053bc9e693
manumounia <doudou.mounia100@gmail.com>
2023-06-16 14:10:55 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-        char num;
+	char num;
 
-        for (num = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '9'; num++)
 {
-        putchar(num);
+	putchar(num);
 }
-        for (num = 'a'; num <= 'f'; num++)
+	for (num = 'a'; num <= 'f'; num++)
 {
-        putchar(num);
+	putchar(num);
 }
-        putchar('\n');
-        return  (0);
+	putchar('\n');
+	return (0);
 
