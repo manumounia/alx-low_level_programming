87caa87464e728bb65704bdd66e39192bcf94cc5
manumounia <doudou.mounia100@gmail.com>
2023-06-16 14:08:07 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16
+ * in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char num;
+
+        for (num = '0'; num <= '9'; num++)
+{
+        putchar(num);
+}
+        for (num = 'a'; num <= 'f'; num++)
+{
+        putchar(num);
+}
+        putchar('\n');
+        return  (0);
+
