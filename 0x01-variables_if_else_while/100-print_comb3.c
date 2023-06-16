fd45a2ed6686b808884b135fbe215bcad792b2c0
manumounia <doudou.mounia100@gmail.com>
2023-06-16 15:29:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,25 +4,23 @@
  * Return: ALways 0 (Success)
  */
 int main() {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 8; i++) {
-        for (j = i + 1; j <= 9; j++) {
-            putchar('0' + i);
-            putchar('0' + j);
-            
-            if (i != 8 || j != 9) {
-                putchar(',');
-                putchar(' ');
-            }
-
-            if (j == 9) {
-                i++;
-                j = i;
-            }
-        }
-    }
-
-    return 0;
+	for (i = 0; i <= 8; i++) {
+	for (j = i + 1; j <= 9; j++) {
+	putchar('0' + i);
+	putchar('0' + j);
+	
+	if (i != 8 || j != 9) {
+	putchar(',');
+	putchar(' ');
 }
 
+	if (j == 9) {
+	i++;
+	j = i;
+}
+}
+}
+	return 0;
+}
