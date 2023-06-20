81a461665bc891b14e966fad8ad005b9d86b853d
manumounia <doudou.mounia100@gmail.com>
2023-06-20 12:31:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * this main program prints _putchar
+ * followed by a new line
+ * The program returns 0
+ */
+int main(void)
+{
+	char put_char="_putchar";
+
+	printf("%s \n",put_char);
+	return (0);
+}
