7e2778d29ed3ea94f5c72db890c334d8432c279b
manumounia <doudou.mounia100@gmail.com>
2023-06-22 23:51:45 +0100

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+/*
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _isupper(int c);
+
+#endif
