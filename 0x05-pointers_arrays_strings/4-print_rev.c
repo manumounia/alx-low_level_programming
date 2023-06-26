13ac6b6a204e5c9d90bcf002685de1da3c87879c
manumounia <doudou.mounia100@gmail.com>
2023-06-26 21:13:24 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int len = 0;
+	
+	while (*str != '\0')
+{
+	len++;
+	str++;
+}
+	str--;
+	for (int i = len;i > 0; i--)
+{
+	_putchar(*str);
+	str--;
+}
+	_putchar('\n');
+}
+
