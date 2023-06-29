8058f476d7d2f32fee9ae9cd60a4e6721d2f6939
manumounia <doudou.mounia100@gmail.com>
2023-06-29 18:19:16 +0100

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcat - returns the length of a string
+ * @dest and src : strings
+ * Return: pointer to a string
+ */
+char *_strcat(char *dest, char *src);
+{
+	char dest[12];
+	char src[12];
+
+	strcat(dest,src);
+	
+	return (dest);
+}
