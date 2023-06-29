09854f516d6d47b0e4ab119b5937e2d930e5d1b2
manumounia <doudou.mounia100@gmail.com>
2023-06-29 18:24:35 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,10 +4,10 @@
  * @dest and src : strings
  * Return: pointer to a string
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
-	char dest[12];
-	char src[12];
+	char *dest[12];
+	char *src[12];
 
 	strcat(dest,src);
 	
