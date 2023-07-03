9a191e4a44a30fb74fcc2aef059fb6f5b478ff6d
manumounia <doudou.mounia100@gmail.com>
2023-07-03 21:07:47 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,15 +6,13 @@
  * @b: constant byte
  * @n: bytes
  *
- * Return: 0 (Success).
+ * Return: Always 0 (Success).
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	
 	for (i = 0; i < n; i++)
-{
-	s[i] = b;
-}
+		s[i] = b;
 	return (s);
 }
